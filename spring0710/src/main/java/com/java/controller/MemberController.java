fdbc1943f2e56713b42693127db52570ff0ce8ec
Administrator <Administrator@DESKTOP-RMA9FF4>
2023-07-10 19:26:44 +0900

    23_07_10 회원가입부분

---
+++ spring0710/src/main/java/com/java/controller/MemberController.java
@@ -0,0 +1,30 @@
+package com.java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MemberController {
+
+	@GetMapping("/member/login")
+	public String login() {
+		return "member/login";
+	}
+
+	@GetMapping("/member/step02")
+	public String step02() {
+		return "member/step02";
+	}
+	
+	@GetMapping("/member/step03")    //회원가입 페이지로
+	public String step03() {
+		return "member/step03";
+	}
+	
+	@PostMapping("/member/step03")   //회원가입 저장시키기
+	public String step03(Model model) {
+		return "member/step03";
+	}
+}
