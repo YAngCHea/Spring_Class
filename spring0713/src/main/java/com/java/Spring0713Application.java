457db43b23bd8c9869eecf6e3a2bcafd121f8d7e
Administrator <Administrator@DESKTOP-RMA9FF4>
2023-08-11 16:29:04 +0900

    0713

---
+++ spring0713/src/main/java/com/java/Spring0713Application.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring0713Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring0713Application.class, args);
+	}
+
+}
